85953e326ff5bb286841509c35527c52a6b38ab1
ig <ig866@mail.ru>
2019-07-01 18:46:01 +0300

    full leson

--- barbershop.rb
+++ barbershop.rb
@@ -4,3 +4,13 @@ require 'sinatra'
 get '/' do
   erb :index
 end
+
+post '/' do
+  # user_name, phone, date_time
+  @user_name = params[:user_name]
+  @phone = params[:phone]
+  @date_time = params[:date_time]
+
+  @title = "Thank you!"
+  @message = "Уважаемый #{@user_name}, мы ждём вас #{@date_time}"
+end
