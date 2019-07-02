762f04f676ba3d7d20c3a22fe11ce6cd10cd40eb
ig <ig866@mail.ru>
2019-07-02 18:49:01 +0300

    site + tz

--- barbershop.rb
+++ barbershop.rb
@@ -1,10 +1,10 @@
-
 require 'sinatra'
 
 get '/' do
   erb :index
 end
 
+# спросим Имя, номер телефона и дату, когда придёт клиент.
 post '/' do
   # user_name, phone, date_time
   @user_name = params[:user_name]
@@ -14,13 +14,32 @@ post '/' do
   @title = "Thank you!"
   @message = "Уважаемый #{@user_name}, мы ждём вас #{@date_time}"
 
+  # запишем в файл то, что ввёл клиент
   f = File.open 'users.txt', 'a'
-  f.chmod 777
-  f.write"User:#{@user_name},#{@phone},#{@date_time}"
+  f.write "User: #{@user_name}, phone: #{@phone}, date and time: #{@date_time}.\n"
   f.close
+
   erb :message
-
-
 end
 
+# Добавить зону /admin где по паролю будет выдаваться список тех, кто записался (из users.txt)
 
+# sinatra text file sso
+get '/admin' do
+  erb :admin
+end
+
+post '/admin' do
+  @login = params[:login]
+  @password = params[:password]
+
+  # проверим логин и пароль, и пускаем внутрь или нет:
+  if @login == 'admin' && @password == 'krdprog'
+    @file = File.open("./users.txt","r")
+    erb :result
+    # @file.close - должно быть, но тогда не работает. указал в erb
+  else
+    @report = '<p>Доступ запрещён! Неправильный логин или пароль.</p>'
+    erb :admin
+  end
+end
